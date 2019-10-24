49a88b157c7c980d7ebd93709d637858199ae5c4
Tony0505 <56942624+Tony0505@users.noreply.github.com>
2019-10-23 22:09:34 -0600

    Archivo inicial

---
+++ BlackJack.py
@@ -0,0 +1,45 @@
+form random import randint
+import os
+
+def cartas(): ##Function for define de card
+    Value=["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+
+    Tipo=["C","D","T","P"]
+
+    for i in Tipo:
+        for j in Value:
+            carta.append(j+ "de" +i)
+        return carta
+
+
+def cartasvalor(): ## Function for define de card value
+    if carta[:1] in ("J","Q","K"):
+        return int(10)
+    elif carta[:1] in ("2","3","4","5","6","7","8","9","10"):
+        return int(carta[:1])
+    elif carta[:1] == A
+        print(str(carta))
+        num = input("Elija el valor de su carta 1 o 11")
+        while num != "1" or num != "11"
+            if num =="1":
+                return int(1)
+            elif num == "11":
+                return int(11)
+            else:
+                num = input("Elija el valor de su carta 1 o 11")
+
+def new_card
+
+
+def menu():
+
+    print (
+
+while True:
+
+    menu()
+
+    jugadores= input ("Ingrese el numero de jugadores >> ")
+
+
+    for (a=0;a<jugadores;a++)
