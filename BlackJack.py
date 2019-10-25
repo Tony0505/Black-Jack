07bd1966c043bbc862b38361628a9ea24a28a329
Tony0505 <56942624+Tony0505@users.noreply.github.com>
2019-10-24 20:50:07 -0600

    Archivo Final

--- BlackJack.py
+++ BlackJack.py
@@ -1,4 +1,4 @@
-form random import randint
+from random import randint
 import os
 
 def deck(): ##Function for define de card
@@ -8,71 +8,103 @@ def deck(): ##Function for define de card
 
     for i in Tipo:
         for j in Value:
-            card.append(j+ "of" +i)
-        return card
+            card.append(j+ "of" + i +" ")
+    return card
 
 
 def value(cardvalue): ## Function for define de card value
-    if cardvalue[:1] in ("J","Q","K"):
+    if cardvalue[:1] in ("J","Q","K","1"):
         return int(10)
     elif cardvalue[:1] in ("2","3","4","5","6","7","8","9","10"):
         return int(cardvalue[:1])
-    elif cardvalue[:1] == A
-        print(str(cartavalue))
-        num = input("Chose the value of your card 1 or 11")
-        while num != "1" or num != "11"
+    elif cardvalue[:1] == "A":
+        print(str(cardvalue))
+        num = input("Chose the value of your card 1 or 11 >>")
+        while num != "1" or num != "11":
             if num =="1":
                 return int(1)
             elif num == "11":
                 return int(11)
             else:
-                num = input("Elija el valor de su carta 1 o 11")
+                num = input("Chose the value of your card 1 or 11 >>")
 
-def new_card(card):
-    return card[randint(0,len(deck)-1)]
+def new_card(card): #Function for new card random
+    return card[randint(0,len(card)-1)]
 
-def remove_card(card,cardvalue):
+def remove_card(card,cardvalue): #Function for remove card of the deck
     return card.remove(cardvalue)
 
-def first_hand(player)
-    new_deck=card_deck()
-    card1=new_card(new_deck)
-    remove_card(new_deck,card1)
-    card2 = new_card(new_deck)
-    remove_card(new_deck,card2)
-    V[player]=[]
+while True: #Inicio de programa
 
-a={}
-def num_players():
-    players= int (input ("Number of players"))
+    print ("\n\n Bienvenido a Black-Jack online \n")
+    
+    Players= int (input ("Ingrese el numero de jugadores >> "))
 
-    for i=0;i<players;i++ :
-        key=
+    new_deck=deck()
+    playerCards = {}
+    playerValue = {}
+    for i in range(Players): #First hand, two cards for all the players
         
-    
+        card1=new_card(new_deck)
+        remove_card(new_deck,card1)
+        card2 = new_card(new_deck)
+        remove_card(new_deck,card2)
 
-def menu():
-
-    print (
-
-while True:
-
-    menu()
-
-    Players= input ("Ingrese el numero de jugadores >> ")
-
-
-    playerList = []
+        value1=value(card1)
+        value2=value(card2)
+        
+        totalv = value1+value2
+        totalc = card1+card2
+        
+        playerCards[i] = totalc
+        playerValue[i] = totalv
+        
     for i in range(Players):
-        playerList[i] = ["Jugador"+i]
+        print ("\n Player number ",i)
+        print (playerCards[i])
+        print (playerValue[i])
 
-    #Primera Mano
-    for i in range(Players)
-        player=
+    ##Second Hand, the player decide how much cards he want
+        
+    for i in range(Players): ##Loop for more cards
+        print ("\n The player ", i , " wants more cards?")
+        m=int(input ("Press 1 for more cards or 0 for pass "))
+
+        if m==1:
+            while True: ##Loop for add the new cards
+                extracard=new_card(new_deck)
+                remove_card(new_deck,extracard)
+                extravalue=value(extracard)
+
+                totalv = playerValue[i]+extravalue
+                totalc = playerCards[i]+extracard
+
+                playerCards[i] = totalc
+                playerValue[i] = totalv
+
+                print ("The new card is ",extracard, "and you have a total of ",totalv)
+
+                m=int(input ("\n Pres 1 for more cards or 0 for pass "))
+
+                if m==1:
+                    continue
+                if m==0:
+                    break
+                
+    plwin=0
+    mx=0
+    for i in range(Players): ##loop for define the winner
+        if playerValue[i]<22:
+            if mx<playerValue[i]:
+                mx=playerValue[i]
+                plwin=i
+
+    print ("\n All the cards by player")
     
+    for i in range(Players):
+        print ("\n Player number ",i)
+        print (playerCards[i])
+        print (playerValue[i])
+        
+    print ("\n CONGRATULATIONS \n The winner is player number", plwin)    
 
-
-
-
-
-    
